ed591c6519bd447d40b018c23557d891bd7e639c
fentacoder <jamar.phillip99@gmail.com>
2020-08-26 16:36:24 -0400

    version 1.0.0

--- src/main/java/com/infotechnano/springboottutorial/InfotechnanoSpringBootTutorialApplication.java
+++ src/main/java/com/infotechnano/springboottutorial/InfotechnanoSpringBootTutorialApplication.java
@@ -1,13 +1,13 @@
-package com.infotechnano.vidtogo.vidtogo;
+package com.infotechnano.springboottutorial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VidtogoApplication {
+public class InfotechnanoSpringBootTutorialApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(VidtogoApplication.class, args);
+		SpringApplication.run(InfotechnanoSpringBootTutorialApplication.class, args);
 	}
 
 }
